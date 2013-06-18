3ab953288b2fb35c54255f3c75eca5aeb9ee1282
Bruce <kenkyojojo@gmail.com>
2013-06-18 11:21:05 +0800

    To debug daily_copy.sh change the safelog.fileattr..err.tar.gz name

--- dailycheck/daily_copy.sh
+++ dailycheck/daily_copy.sh
@@ -97,7 +97,7 @@ STEP3() {
 		if [ $CHKSTATUS -gt 0 ];then
     		cp  $LOGDIR/safelog.$hostname.fileattr.$DATE $SELOG/log/ 2>/dev/null
 		fi
-			tar -cf - $SELOG/log/safelog.*.fileattr.$DATE  | gzip  > $SELOG/log/safelog.*.fileattr.$DATE.err.tar.gz 2>/dev/null
+			tar -cf - $SELOG/log/safelog.*.fileattr.$DATE  | gzip  > $SELOG/log/safelog.fileattr.$DATE.err.tar.gz 2>/dev/null
 			find $SELOG/log/ -type f -mtime +14 -name "safelog.*.fileattr.*.err.tar.gz" -exec rm {} \;
 			rm -f $SELOG/log/safelog.*.fileattr.$DATE 2>/dev/null
 	fi
