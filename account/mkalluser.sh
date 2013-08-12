fe96708db93eae2c3f365e0e4145a7db0811b72e
Bruce <kenkyojojo@gmail.com>
2013-08-12 19:10:37 +0800

    modified:account/mkalluser.sh change the user account

--- account/mkalluser.sh
+++ account/mkalluser.sh
@@ -10,11 +10,15 @@ cd /home/se/safechk/safesh/account
 
 ./mkuser.sh exc exc 002 600 exadm
 
+./mkuser.sh exc exc 002 603 ex03
+
+./mkuser.sh exc exc 002 610 ex10
+
 ./mkuser.sh exc exc 002 611 ex11
 
 ./mkuser.sh exc exc 002 614 ex14
 
-./mkuser.sh exc exc 002 616 ex16
+./mkuser.sh exc exc 002 615 ex15
 
 ./mkuser.sh tse tse 007 800 twse
 
