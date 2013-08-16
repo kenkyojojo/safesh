7a47edceb79063c3153e3354f168f6fb772a6547
Bruce <kenkyojojo@gmail.com>
2013-08-16 18:25:11 +0800

    modified: dailycheck/daily_check.sh, security_summar_chk.sh, syschk_diff.sh

--- syschk_diff.sh
+++ syschk_diff.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/ksh
 SHELL=/home/se/safechk/safesh
 LOG=/home/se/safechk/file/syschk
+CHKLOG=/home/se/safechk/safelog
 DT=`date +"%y/%m/%d %H:%M:%S"`
 HOSTNAME=`hostname`
 
-$SHELL/syschk_current.sh > $LOG/err/syschk_diff.errlog  2>&1
+$SHELL/syschk_current.sh 
 diff $LOG/base/syschk.${HOSTNAME}.base $LOG/check/syschk.${HOSTNAME}.now > $LOG/result/syschk.diff.`date +"%Y%m%d"` 2> $LOG/err/syschk_diff.errlog
 
 echo "########################################################" >> $LOG/syschk_diff.log
 echo "Start Time $DT" >> $LOG/syschk_diff.log
 
 if [[ -s $LOG/result/syschk.diff.`date +"%Y%m%d"` ]];then
-	echo "Hostname:$HOSTNAME  Error" >> $LOG/syschk_diff.log
-	echo "Hostname:$HOSTNAME  Please to check the file $LOG/result/syschk.diff.`date +"%Y%m%d"`" >> $LOG/syschk_diff.log
-	tail -4 $LOG/syschk_diff.log
+	cp $LOG/result/syschk.diff.`date +"%Y%m%d"` $LOG/result/syschk.diff
+	echo "$HOSTNAME  Error Please to check the file $LOG/result/syschk.diff " >> $LOG/syschk_diff.log
+	tail -3 $LOG/syschk_diff.log > $CHKLOG/syschk.log
 else
-	echo "Hostname:$HOSTNAME  OK." >> $LOG/syschk_diff.log
-	tail -3 $LOG/syschk_diff.log
+	echo "$HOSTNAME  OK." >> $LOG/syschk_diff.log
+	tail -3 $LOG/syschk_diff.log > $CHKLOG/syschk.log
 fi
-
