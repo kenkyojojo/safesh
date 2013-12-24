20af6a6b4dae1a6233bfa5474da8dae7809fd478
Bruce <kenkyojojo@gmail.com>
2013-12-24 20:39:39 +0800

    modified:fix the filecheck.sh site name,security_summar_chk.sh to change sleep time

--- dailycheck/fileaudit/filecheck.sh
+++ dailycheck/fileaudit/filecheck.sh
@@ -20,7 +20,7 @@
 #----------------------------------
 hostname=`hostname`
 SHELL=filecheck.sh
-SITE=TSEOT1
+SITE=TSEOA1
 CONFIGDIR=/home/se/safechk/cfg
 FILEDIR=/home/se/safechk/file/fileaudit
 LOGDIR=/home/se/safechk/safelog
