a2075a267cb4e3d196d7c7fc7e13470ad92fc82b
Bruce Hsiao <kenkyojojo@gmail.com>
2014-05-23 09:46:29 +0800

    modified:filecheck.sh, genbas_file_attr.sh

--- dailycheck/fileaudit/genbas_file_attr.sh
+++ dailycheck/fileaudit/genbas_file_attr.sh
@@ -6,6 +6,7 @@
 hostname=`hostname`
 HOSTN=$(echo $hostname | cut -c 1-3)
 CONFIGDIR=/home/se/safechk/cfg
+SHDIR=/home/se/safechk/safesh
 case $HOSTN in
 	DAP)
 		DIR=`head -1 $CONFIGDIR/dir.conf.dap`
@@ -42,6 +43,13 @@ case $HOSTN in
 		NOCHECK=`sed -n '2p' $CONFIGDIR/dir.conf.wkl;sed -n '3p' $CONFIGDIR/dir.conf.wkl;sed -n '4p' $CONFIGDIR/dir.conf.wkl`
 		EXCLUDE=`tail -1 /home/se/safechk/cfg/dir.conf.wkl | sed -e 's#\/#\\\/#g' -e 's/ /\/d\" -e \"\//g' -e 's/^/sed -e "\//' -e 's/$/\/d"/'`
 		;;
+#	X23)
+#		DIR=`head -1 $CONFIGDIR/dir.conf.x230`
+#		EXIST=`sed -n '2p' $CONFIGDIR/dir.conf.x230`
+#		DIRNOTIME=`sed -n '3p' $CONFIGDIR/dir.conf.x230`
+#		NOCHECK=`sed -n '2p' $CONFIGDIR/dir.conf.x230;sed -n '3p' $CONFIGDIR/dir.conf.x230;sed -n '4p' $CONFIGDIR/dir.conf.x230`
+#		EXCLUDE=`tail -1 /home/se/safechk/cfg/dir.conf.x230 | sed -e 's#\/#\\\/#g' -e 's/ /\/d\" -e \"\//g' -e 's/^/sed -e "\//' -e 's/$/\/d"/'`
+#		;;
 	*)
 		DIR=`head -1 $CONFIGDIR/dir.conf`
 		EXIST=`sed -n '2p' $CONFIGDIR/dir.conf`
@@ -66,7 +74,8 @@ BACKUP_EXISTBASE=$FILEDIR/base/${hostname}_previous_file_exist.bas
 creat_base(){
 for DIRNAME in $DIR #import all dir_list from prompt
 do
-   find $DIRNAME -ls | eval $ALLEXCLUDE | sort -k2  >> $BASEFILE #generate CURRENT status to compare with BASEFILE
+   #${SHDIR}/sefind $DIRNAME 2>/dev/null | eval $ALLEXCLUDE | sort -k2  >> $BASEFILE #generate CURRENT status to compare with BASEFILE
+   find $DIRNAME -ls 2>/dev/null | eval $ALLEXCLUDE | sort -k2  >> $BASEFILE #generate CURRENT status to compare with BASEFILE
 done
 }
 
@@ -74,12 +83,14 @@ done
 creat_existbase(){
 for DIRNAME in $EXIST #import all dir_list from prompt
 do
-   ls -ld  $DIRNAME  2> /dev/null | eval $EXCLUDE |awk '{print $3,$4,$1,$9}' >> $EXISTBASE
+	#${SHDIR}/sels  $DIRNAME  2>/dev/null | eval $EXCLUDE |awk '{print $3,$4,$1,$9}' >> $EXISTBASE
+    ls  -ld $DIRNAME  2>/dev/null | eval $EXCLUDE |awk '{print $3,$4,$1,$9}' >> $EXISTBASE
 done
 
 for DIRNAME in $DIRNOTIME #Recursive list dir, but don't list the file and directory time. 
 do
-   find $DIRNAME -ls | eval $EXCLUDE | awk '{print $5,$6,$3,$NF}'  >> $EXISTBASE
+	#${SHDIR}/sefind $DIRNAME 2>/dev/null | eval $EXCLUDE | awk '{print $5,$6,$3,$NF}'  >> $EXISTBASE
+    find $DIRNAME  -ls 2>/dev/null | eval $EXCLUDE | awk '{print $5,$6,$3,$NF}'  >> $EXISTBASE
 done
 }
 
