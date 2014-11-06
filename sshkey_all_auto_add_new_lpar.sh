5598a9a96ee02507bbf106519c6e228f45f99796
Bruce Hsiao <kenkyojojo@gmail.com>
2014-11-06 10:13:53 +0800

    On branch TSEOB1 Changes to be committed:   (use "git reset HEAD <file>..." to unstage)
    	modified:   sshkey_all_auto.sh
    	new file:   sshkey_all_auto_add_new_lpar.sh
    	new file:   sshkey_auto_exchange_fixgw.sh
    	new file:   sshkey_auto_exchange_otcfixgw.sh

--- sshkey_all_auto_add_new_lpar.sh
+++ sshkey_all_auto_add_new_lpar.sh
@@ -12,7 +12,6 @@
 # Set variable
 #----------------------------------
 WLPAR=`hostname`
-#USERLIST="useradm seadm se02 exadm twse"
 set -A USERLIST `cat /home/se/safechk/cfg/sshkey_user.lst`
 
 PASSWORD=1234567
@@ -44,15 +43,12 @@ if [ "$PASSWORD" == "$PASSWORD1" ]; then
       GROUP=`lsuser $USER | awk '{print $3}' | cut -c6-`
       SSHDIR="$HOMEDIR/.ssh"
       cd $SSHDIR
-	  rm -f known_hosts
-      cat /dev/null > authorized_keys
+      cat id_rsa.pub >> authorized_keys
       cat id_rsa.pub.*.${USER} >> authorized_keys
       chmod 600 authorized_keys
       chown ${USER}:${GROUP} authorized_keys
       rm id_rsa.pub.*.${USER}
    done
-   cd $SSHDIR
-   cat authorized_keys | sort -u -o authorized_keys
 
    #----------------------------------
    # sync authorized_key file
